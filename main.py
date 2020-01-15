adb246993beabd1abe1ec4377205965d4ea57da4
笹倉秀行 <sasakurahideyuki@sasakurahideyuki-no-MacBook-Air.local>
2020-01-15 15:26:16 +0900

    ready updated files

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 # main.py:
 from debugmodule import *
+
 import teama.member1
+import teama.member2
+
+import teamb.member1
+import teamb.member2
+
+import teamc.member1
+import teamc.member2
+
+import teamd.member1
+import teamd.member2
+
+import teame.member1
+import teame.member2
+
 
 def callFirst():
 	start()
@@ -10,6 +25,30 @@ def callLast():
 
 if __name__ == '__main__':
 	callFirst()
+
 	teama.member1.process1()
 	teama.member1.process2()
+	teama.member2.process1()
+	teama.member2.process2()
+
+	teamb.member1.process1()
+	teamb.member1.process2()
+	teamb.member2.process1()
+	teamb.member2.process2()
+
+	teamc.member1.process1()
+	teamc.member1.process2()
+	teamc.member2.process1()
+	teamc.member2.process2()
+
+	teamd.member1.process1()
+	teamd.member1.process2()
+	teamd.member2.process1()
+	teamd.member2.process2()
+
+	teame.member1.process1()
+	teame.member1.process2()
+	teame.member2.process1()
+	teame.member2.process2()
+
 	callLast()
---
+++ teamc/member2.py
@@ -0,0 +1,11 @@
+# member2.py:
+
+def process1():
+	pass
+	# TODO C2 以下のコメントアウトを外してください！
+	#print("called teama member2 process1")
+
+def process2():
+	pass
+	# TODO C7 以下のコメントアウトを外してください！
+	#print("called teama member2 process2")
